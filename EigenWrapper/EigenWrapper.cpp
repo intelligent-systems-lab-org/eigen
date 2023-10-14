80cdcbe50d8b77aee96295d5e12f29d1138eed87
leebissessar5 <leebissessar@gmail.com>
2023-10-14 18:37:32 -0400

    Use Eigen namespace

--- EigenWrapper/EigenWrapper.cpp
+++ EigenWrapper/EigenWrapper.cpp
@@ -1,57 +1,58 @@
 #include "EigenWrapper.h"
-#include <Eigen/Dense>
+#include <Eigen/Core>
+
+using namespace Eigen;
 
 extern "C" {
     DLLExport void* CreateMatrix(int rows, int cols, float* data)
     {
-        Eigen::MatrixXf* mat = new Eigen::MatrixXf(rows, cols);
+        MatrixXf* mat = new MatrixXf(rows, cols);
         std::memcpy(mat->data(), data, sizeof(float) * rows * cols);
         return mat;
     }
 
     DLLExport void DeleteMatrix(void* matrixPtr)
     {
-        delete static_cast<Eigen::MatrixXf*>(matrixPtr);
+        delete static_cast<MatrixXf*>(matrixPtr);
     }
 
     DLLExport void MultiplyMatrices(void* matrix1, void* matrix2, float* result)
     {
-        Eigen::MatrixXf& mat1 = *static_cast<Eigen::MatrixXf*>(matrix1);
-        Eigen::MatrixXf& mat2 = *static_cast<Eigen::MatrixXf*>(matrix2);
+        MatrixXf& mat1 = *static_cast<MatrixXf*>(matrix1);
+        MatrixXf& mat2 = *static_cast<MatrixXf*>(matrix2);
 
-        Eigen::MatrixXf resultMat = mat1 * mat2;
+        MatrixXf resultMat = mat1 * mat2;
         std::memcpy(result, resultMat.data(), sizeof(float) * resultMat.rows() * resultMat.cols());
     }
 
     DLLExport int GetRowCount(void* matrixPtr) {
-        Eigen::MatrixXf* mat = static_cast<Eigen::MatrixXf*>(matrixPtr);
+        MatrixXf* mat = static_cast<MatrixXf*>(matrixPtr);
         return mat->rows();
     }
 
     DLLExport int GetColumnCount(void* matrixPtr) {
-        Eigen::MatrixXf* mat = static_cast<Eigen::MatrixXf*>(matrixPtr);
+        MatrixXf* mat = static_cast<MatrixXf*>(matrixPtr);
         return mat->cols();
     }
 
     DLLExport float GetElement(void* matrixPtr, int row, int col) {
-        Eigen::MatrixXf* mat = static_cast<Eigen::MatrixXf*>(matrixPtr);
+        MatrixXf* mat = static_cast<MatrixXf*>(matrixPtr);
         return (*mat)(row, col);
     }
 
     DLLExport void SetElement(void* matrixPtr, int row, int col, float newEntry) {
-        Eigen::MatrixXf* mat = static_cast<Eigen::MatrixXf*>(matrixPtr);
+        MatrixXf* mat = static_cast<MatrixXf*>(matrixPtr);
         (*mat)(row, col) = newEntry;
     }
 
     DLLExport void Transpose(void* matrixPtr, float* result) {
-        Eigen::MatrixXf* mat = static_cast<Eigen::MatrixXf*>(matrixPtr);
-        Eigen::MatrixXf transposedMat = mat->transpose();
+        MatrixXf* mat = static_cast<MatrixXf*>(matrixPtr);
+        MatrixXf transposedMat = mat->transpose();
         std::memcpy(result, transposedMat.data(), sizeof(float) * transposedMat.rows() * transposedMat.cols());
     }
 
-
     DLLExport void TransposeInPlace(void* matrixPtr) {
-        Eigen::MatrixXf* mat = static_cast<Eigen::MatrixXf*>(matrixPtr);
+        MatrixXf* mat = static_cast<MatrixXf*>(matrixPtr);
         mat->transposeInPlace();
     }
 }
